e3f77c4ba7a2e141b568a379f9eb9ae68f16a100
yk <yosizuka1@gmail.com>
2014-02-18 00:05:29 +0900

    [AddressBook]ルート見直し(不要箇所削除) 0003

--- lib/AddressBook.pm
+++ lib/AddressBook.pm
@@ -5,7 +5,7 @@ use Teng::Schema::Loader;
 
 sub startup {
     my $self = shift;
-    # model(Teng)
+
     $self->helper( model => sub {
         state $model = Teng::Schema::Loader->load(
             connect_info => ['dbi:SQLite:./lib/AddressBook/Model/address_book.db','',''],
@@ -13,7 +13,7 @@ sub startup {
         );
         return $model;
     });
-    # Router
+
     my $r = $self->routes;
 
     # サイト全体 開発用
@@ -24,13 +24,7 @@ sub startup {
     $r->route('/login')->to(controller => 'Auth', action => 'login');
     $r->route('/logout')->to(controller => 'Auth', action => 'logout');
 
-#     $r->route('/member/create')->to(controller => 'member', action => 'create');
-
-#     $r->get('/welcome')->to(controller => 'example', action => 'welcome');
     $r->route('/all')->to(controller => 'Display', action => 'all');
-#     $r->route('/keyword')->to(controller => 'Display', action => 'keyword');
-#     $r->route('/icon')->to(controller => 'Display', action => 'icon');
-
 }
 
 1;
