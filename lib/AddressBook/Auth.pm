1cd051732054aeb035fe7b14ac25f1e8acb36064
yk <yosizuka1@gmail.com>
2014-09-27 15:46:42 +0900

    ログイン画面の作成(templates切替)

--- lib/AddressBook/Auth.pm
+++ lib/AddressBook/Auth.pm
@@ -4,80 +4,84 @@ use Readonly;
 use FormValidator::Lite qw{Email};
 use HTML::FillinForm;
 use Data::Dumper;
+
 sub login {
     my $self = shift;
-    my $model = $self->model;
-    # ログイン確認
-    if (defined $self->session->{name}) {
-        return $self->redirect_to('/');
-    }
-    my $res = +{
-        mail => undef,
-        pass => undef,
-        message => undef,
-    };
-    # postリクエストのみ,getは無視
-    if (uc $self->req->method eq 'POST') {
-        # パラメーターを全て取得
-        my $params = $self->req->params->to_hash;
-        # バリデータのインスタンスを作成
-        my $validator = FormValidator::Lite->new($self->req);
-        # 入力値の存在確認
-        my $res = $validator->check(
-            mail => ['REQUIRED', 'EMAIL', 'NOT_NULL'],
-            pass => ['REQUIRED', 'NOT_NULL'],
-        );
-        # バリデータ不合格の処置
-        if ($validator->has_error) {
 
-            $self->stash(message => Dumper($validator->errors()),);
+    # my $model = $self->model;
+    # # ログイン確認
+    # if (defined $self->session->{name}) {
+    #     return $self->redirect_to('/');
+    # }
+    # my $res = +{
+    #     mail => undef,
+    #     pass => undef,
+    #     message => undef,
+    # };
+    # # postリクエストのみ,getは無視
+    # if (uc $self->req->method eq 'POST') {
+    #     # パラメーターを全て取得
+    #     my $params = $self->req->params->to_hash;
+    #     # バリデータのインスタンスを作成
+    #     my $validator = FormValidator::Lite->new($self->req);
+    #     # 入力値の存在確認
+    #     my $res = $validator->check(
+    #         mail => ['REQUIRED', 'EMAIL', 'NOT_NULL'],
+    #         pass => ['REQUIRED', 'NOT_NULL'],
+    #     );
+    #     # バリデータ不合格の処置
+    #     if ($validator->has_error) {
 
-            my $html = $self->render(
-                template => 'auth/login',
-                partial => 1,
-            )->to_string;
+    #         $self->stash(message => Dumper($validator->errors()),);
 
-            $html = HTML::FillInForm->fill(\$html, $params,);
+    #         my $html = $self->render(
+    #             template => 'auth/login',
+    #             partial => 1,
+    #         )->to_string;
 
-            return $self->render(
-                template => 'auth/admin_login',
-                text => $html,
-                format => 'html'
-            );
-        }
-        # バリデータ合格の処置 存在確認
-        my $admin = $model->single('admin', +{
-            mail     => $params->{mail},
-            password => $params->{pass},
-        });
-        # 存在の場合、セッション書き込みトップ画面遷移
-        if (defined $admin) {
-            $self->session(+{
-                mail => $admin->mail,
-                name => $admin->name,
-            });
-            return $self->redirect_to('/');
-        }
-        else { # 存在しない場合、メッセージ
+    #         $html = HTML::FillInForm->fill(\$html, $params,);
 
-            $self->stash(message => 'Not exist!',);
+    #         return $self->render(
+    #             template => 'auth/admin_login',
+    #             text => $html,
+    #             format => 'html'
+    #         );
+    #     }
+    #     # バリデータ合格の処置 存在確認
+    #     my $admin = $model->single('admin', +{
+    #         mail     => $params->{mail},
+    #         password => $params->{pass},
+    #     });
+    #     # 存在の場合、セッション書き込みトップ画面遷移
+    #     if (defined $admin) {
+    #         $self->session(+{
+    #             mail => $admin->mail,
+    #             name => $admin->name,
+    #         });
+    #         return $self->redirect_to('/');
+    #     }
+    #     else { # 存在しない場合、メッセージ
 
-            my $html = $self->render(
-                template => 'auth/login',
-                partial => 1
-            )->to_string;
+    #         $self->stash(message => 'Not exist!',);
 
-            $html = HTML::FillInForm->fill(\$html, $params);
+    #         my $html = $self->render(
+    #             template => 'auth/login',
+    #             partial => 1
+    #         )->to_string;
 
-            return $self->render(
-                template => 'auth/login',
-                text => $html,
-                format => 'html',
-            );
-        }
-    }
+    #         $html = HTML::FillInForm->fill(\$html, $params);
 
-    $self->stash(message => $res->{message},);
+    #         return $self->render(
+    #             template => 'auth/login',
+    #             text => $html,
+    #             format => 'html',
+    #         );
+    #     }
+    # }
+
+    # $self->stash(message => $res->{message},);
+
+    # 画面の描写を明示的に
     return $self->render(template => 'auth/login');
 }
 
